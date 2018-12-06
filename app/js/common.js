5917fa45b61bd2f78760dc25f446e00288133db3
klymovy4 <klymovy4roman@gmail.com>
2018-12-07 00:01:00 +0200

    home header media slideToggle

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,18 @@
 $(function() {
 
 $('.header-links-menu').click(function(){
-	$('.header-links-menu-ul').slideToggle(100, function(){
+	$('.header-links-menu-ul').slideToggle(300, function(){
 		if($(this).css('display') === 'none'){
 			$(this).removeAttr('style');
 		}
 	});
 	// alert(123);
 })
-
+$(window).on('scroll', function(){
+    if($(window).scrollTop()){
+        $('header').addClass('black-header'), 2000;
+    }else{
+        $('header').removeClass('black-header');
+    }
+});
 });
